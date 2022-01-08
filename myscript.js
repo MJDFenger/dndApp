79e0ec695c5255702c5aff8e95ae5e19857113ca
MJDFenger <94479781+MJDFenger@users.noreply.github.com>
2022-01-08 11:25:36 -0800

    Add files via upload
    functional code and some CSS.

--- myscript.js
+++ myscript.js
@@ -1,47 +1,113 @@
 let spellDiv;
+let classBoxes;
+let schoolBoxes;
 let message = "";
 let schoolSave ={};
+let spellsList = [];
 let levelCount = -1;
 let settingLevel = 0;
-let spellsList = [];
+let schoolsPicked = "";
+let spellsOfSchools = [];
+let classSpellsList = [];
+let parameterCount = 0; //check how many parameters are used
 
 window.onload = function () {
 	spellDiv = document.getElementById("spells");
 	closeLightBox();
 	getInfo();
 	fillPage();
+	schoolBoxes = document.getElementsByClassName("schoolbox");
+	classBoxes = document.getElementsByClassName("classbox");
 	document.addEventListener("keyup", function(event) {
+		//loads the spells on key press, find a better way to do this.
 	  if (event.keyCode === 13) {
 	   event.preventDefault();
 	   fillPage();
 		}
+	
 	});
+	
 }
-function getInfo(){
-	 
+function getInfo(parameter,type){
+	
+	if(type && type == "c"){
+				//constructs a url for a class' spell list and gets it sorted
+				toFetch = "https://www.dnd5eapi.co/api/classes/";
+				toFetch += parameter;
+				toFetch += "/spells/"
+				fetch(toFetch)
+				.then(response => response.json())
+				.then(data => sortSpells(data,parameter,type))
+				;
+			}
+	else{
+		levelCount = -1;
 		 while (levelCount < 9) {
 			levelCount++;
-			fetch('https://www.dnd5eapi.co/api/spells?level=' + levelCount)
+			let toFetch = "https://www.dnd5eapi.co/api/spells?level=";
+			toFetch += levelCount;
+			if(type && type == "s"){
+				//makes a fetch request 
+				toFetch += "&school=" + parameter;
+			}
+					
+			fetch(toFetch)
 			.then(response => response.json())
-			.then(data => levelSpells(data))
-			.then(console.log(levelCount))
+			.then(data => checkLevel(data,parameter,type))
 			;
-			
 		}
+	}
+}
+
+function spellsByParameters(){
+	parameterCount = 0;
+	spellsOfSchools = [];
+	classSpellsList = [];
+	schoolsPicked = "";
+	for(classx in classBoxes){
+		//each class gets a fetch for its spell list and counts as one parameter
+		if(classBoxes[classx].checked == true){
+			getInfo(classBoxes[classx].value,"c");
+			parameterCount++;
+		}
+	}
+	for(schoolx in schoolBoxes){
+		if(schoolBoxes[schoolx].checked == true){
+			console.log(schoolBoxes[schoolx].value);
+			schoolsPicked += schoolBoxes[schoolx].value;
+			schoolsPicked += ",";
+		}
+	}
+	if(schoolsPicked){
+		//if the user is filtering by school, do a search for that.
+		//
+		//parameterCount = parameterCount + 10;
+		schoolsPicked = schoolsPicked.slice(0, -1);
+		getInfo(schoolsPicked,"s");
+	}else{ getInfo();}
 }
 
 function levelSpells(data){
 	console.log(data);
-	level = checkLevel(data.results);
-	let spellsAdded = "";
-	spellsAdded += "<h2>" + level + "</h2>"; 
-	for (spell in data.results) {
-		let output = "";
-		output += "<a href='javascript:getSpell(\"" + data.results[spell].url + "\")'>";
-		output += data.results[spell].name + "</a> <br>";
-		spellsAdded += output;
+	
+	let levelCounter = 0;
+	for (levelListed in data){
+		let spellsAdded = "";
+		if(data[levelListed].length > 0){
+			spellsAdded += "<h2>" + levelListed + "</h2>"; 
+			console.log(levelListed);
+			for (spell in data[levelListed]) {
+				let output = "";
+				let spellID = data[levelListed][spell]
+				output += "<a href='javascript:getSpell(\"" + spellID.url + "\")'>";
+				output += spellID.name + "</a> <br>";
+				spellsAdded += output;
+			}
+		}
+		spellsList[levelCounter] = (spellsAdded);
+		levelCounter++;
 	}
-	spellsList[level] = (spellsAdded);
+	fillPage();
 }
 
 function fillPage(){
@@ -52,7 +118,7 @@ function fillPage(){
 }
 
 function getSpell(spellUrl){
-	
+	console.log('https://www.dnd5eapi.co' + spellUrl);
 	fetch('https://www.dnd5eapi.co' + spellUrl)
 	.then(response => response.json())
 	.then(data => showSpell(data));
@@ -146,29 +212,55 @@ function showLightbox(message){
      document.getElementById("lightbox").style.display="none";
  } // closeLightBox 
  
-function checkLevel(data){
-	//improve later
-	if (data[0].index == "acid-splash"){
-		return 0;
+function checkLevel(data,parameter,type){
+	//gets the url of the first spell
+	let spellData = data;
+	if(data.results.length > 0){
+		let urlEnd = data.results[0].url;
+		//Looks up the first spell of a set to find its level
+		parameterCount++;
+		fetch('https://www.dnd5eapi.co' + urlEnd)
+		.then(response => response.json())
+		.then(data => sortSpells(spellData,parameter,type,data.level))
+		;
 	}
-	if (data[0].index == "alarm"){
-		return 1;
+}
+
+function sortSpells(spellData,parameter,type,levelData){
+	//if the API allowed 'class' as a parameter in requests for spells, we wouldn't be here.
+	if(!type || type =="s"){
+		spellsOfSchools[levelData] = spellData.results;
+		parameterCount--;
+	}else if(type&&type=="c"){
+		for(spell in spellData.results){
+			classSpellsList.push(spellData.results[spell]);
+		}
+		parameterCount--;
 	}
-	if (data[0].index == "acid-arrow"){
-		return 2;
-	}if (data[0].index == "animate-dead"){
-		return 3;
-	}if (data[0].index == "arcane-eye"){
-		return 4;
-	}if (data[0].index == "animate-objects"){
-		return 5;
-	}if (data[0].index == "blade-barrier"){
-		return 6;
-	}if (data[0].index == "arcane-sword"){
-		return 7;
-	}if (data[0].index == "animal-shapes"){
-		return 8;
-	}if (data[0].index == "astral-projection"){
-		return 9;
+	if (parameterCount == 0){
+		let spellsToProcess = [];
+		if(classSpellsList.length > 0){	
+			for(levelListed in spellsOfSchools){
+				spellsToProcess[levelListed] = [];
+				//spellsToProcess[levelListed] = spellsOfSchools.filter(x => classSpellsList.includes(x));
+				//spellsToProcess[levelListed] = spellsOfSchools[levelListed].filter(classIntersect);
+				for(spell in spellsOfSchools[levelListed]){
+					if(classSpellsList.find(el => el.index === spellsOfSchools[levelListed][spell].index)){
+						spellsToProcess[levelListed].push(spellsOfSchools[levelListed][spell]);
+					}
+				}
+			}
+		}else{
+			spellsToProcess = spellsOfSchools;
+			}
+			
+		levelSpells(spellsToProcess);
 	}
+	
+}
+
+function classIntersect(el){
+	if (classSpellsList.find(el => el.index === spellsOfSchools[levelListed][spell].index)){
+		return true;
+	}else { return false;}
 }
